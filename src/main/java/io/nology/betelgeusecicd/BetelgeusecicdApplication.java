9a4688f59876360c4bdcdf81c1108b53a0bfbef6
Calum Hill-Cawthorne <calum@nology.io>
2022-06-22 12:12:47 +1000

    updated to using 2 space indents

--- src/main/java/io/nology/betelgeusecicd/BetelgeusecicdApplication.java
+++ src/main/java/io/nology/betelgeusecicd/BetelgeusecicdApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BetelgeusecicdApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BetelgeusecicdApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(BetelgeusecicdApplication.class, args);
+  }
 
 }
