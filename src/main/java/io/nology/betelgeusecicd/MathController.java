cb0e94680f9958465b41cc7b3aa1eb704fb730ed
Calum Hill-Cawthorne <calum@nology.io>
2022-06-22 12:09:26 +1000

    fixed code styling

--- src/main/java/io/nology/betelgeusecicd/MathController.java
+++ src/main/java/io/nology/betelgeusecicd/MathController.java
@@ -8,20 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/math")
 public class MathController {
-	// 1. Defining Path rules, data arguments
-	// 2. Define DTOs
-	// 3. Validate DTOs
-	// 4. Consolidate argument data (queries, path variables)
-	// 5. issue appropriate responses
-	// 6. delegating where data should go (what services we are using, what methods we are using)
-	
-	@GetMapping(value = "/rectanglePerimeter/{height}/{width}")
-	public Float rectangle_perimeter(@PathVariable Float height, @PathVariable Float width) {
-		return (height + width) * 2;
-	}
-	
-	@GetMapping(value = "circleArea/{radius}")
-	public Float circleArea(@PathVariable Float radius) {
-		return (float)(3.14 * Math.pow(radius, 2)); 
-	}
+    // 1. Defining Path rules, data arguments
+    // 2. Define DTOs
+    // 3. Validate DTOs
+    // 4. Consolidate argument data (queries, path variables)
+    // 5. issue appropriate responses
+    // 6. delegating where data should go (what services we are using, what methods we are using)
+    
+    @GetMapping(value = "/rectanglePerimeter/{height}/{width}")
+    public Float rectanglePerimeter(@PathVariable Float height, @PathVariable Float width) {
+        return (height + width) * 2;
+    }
+    
+    @GetMapping(value = "circleArea/{radius}")
+    public Float circleArea(@PathVariable Float radius) {
+        return (float)(3.14 * Math.pow(radius, 2)); 
+    }
 }
--- src/test/java/io/nology/betelgeusecicd/MathControllerTests.java
+++ src/test/java/io/nology/betelgeusecicd/MathControllerTests.java
@@ -7,22 +7,22 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class MathControllerTests {
-	@Test
-	public void testRectanglePerimeterReturnsRightPerimeter() {
-		MathController controller = new MathController();
-		
-		Float result = controller.rectangle_perimeter(100f, 200f);
-		Float output = 600.0f;
-		
-		assertTrue(output.equals(result));
-	}
-	
-	@Test
-	public void testCircleAreaReturnsRightArea() {
-		MathController controller = new MathController();
-		Float result = controller.circleArea(10f);
-		Float output = 314.0f;
-		
-		assertTrue(output.equals(result));
-	}
+    @Test
+    public void testRectanglePerimeterReturnsRightPerimeter() {
+        MathController controller = new MathController();
+        
+        Float result = controller.rectanglePerimeter(100f, 200f);
+        Float output = 600.0f;
+        
+        assertTrue(output.equals(result));
+    }
+    
+    @Test
+    public void testCircleAreaReturnsRightArea() {
+        MathController controller = new MathController();
+        Float result = controller.circleArea(10f);
+        Float output = 314.0f;
+        
+        assertTrue(output.equals(result));
+    }
 }
